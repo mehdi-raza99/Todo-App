77e98f5b3bee6397d3e145330fa8dd59c6cd74cd
Mehdi Raza <mehdics100@gmail.com>
2025-03-02 07:52:10 +0500

    All functionalities completed

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,15 @@ function App() {
       description: task
     }
   setTaskList([...taskList,newTask]);
-   console.log(taskList);
+  }
+  function saveEdit(Key,updatedTask){
+    console.log(Key);
+    console.log(updatedTask);
+    
+    const updatedList=taskList.map((task) => task.id === Key? {...task,description: updatedTask} : task)
+    setTaskList(updatedList);
+    console.log(updatedList);
+    
   }
   function deleteTask(id) {
     const updatedList=taskList.filter((task) => task.id !== id)
@@ -20,9 +28,9 @@ function App() {
   }
   return (
     <div className='w-[1080px] mx-auto py-14 '>
-      <div className='text-5xl font-serif mx-auto mb-10 text-blue-700 bg-emerald-300 rounded-2xl max-w-fit p-5'>TODO APP</div>
+      <div className='text-5xl font-serif mx-auto mb-10 text-blue-700 bg-emerald-300 rounded-2xl max-w-fit p-5 tracking-widest'>TODO APP</div>
       <Search addTask={addNewTask} />
-      {taskList.map((task) => <Task key={task.id} Key={task.id} task={task.description} handleDelete={deleteTask} />)}
+      {taskList.map((task) => <Task key={task.id} Key={task.id} task={task.description} handleDelete={deleteTask} update={saveEdit} />)}
     </div>
   )
 }
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
-
-const Task = ({task, Key, handleDelete}) => {
+import React, { useState } from 'react'
+const Task = ({task, Key, handleDelete, update}) => {
+  const [editText, setEditText] = useState(task);
+  const [isEditing, setIsEditing] = useState(false);
+  function handleEditChange(e) {
+    setEditText(e.target.value)
+  }
+  
+  function handleEdit(id) {
+    setIsEditing(true)
+  }
   return (
+    
     <div className='flex gap-3 place-items-center'>
+    {isEditing? (
+      <form className='flex gap-2 place-items-center mt-3' onSubmit={() => {update(Key,editText); setIsEditing(false)}}>
+        <input
+          type="text"
+          value={editText}
+          onChange={handleEditChange}
+          className='text-2xl bg-gray-300 border-2 rounded-xl p-2 w-[40rem]'
+          autoFocus
+        />
+        <button type='submit' className='bg-yellow-400 p-3 rounded-lg cursor-pointer font-medium text-lg'>Save</button>
+      </form>
+    ) : (
+      <>
     <div className='my-3 text-2xl bg-black border-2 border-amber-100 rounded-xl p-2 w-[40rem] text-gray-400'>
       {task}
     </div>
+    <button onClick={() => handleEdit(Key)} className='bg-yellow-400 p-3 rounded-lg cursor-pointer font-medium text-lg'>Edit</button>
     <button onClick={() => handleDelete(Key)} className='bg-red-500 p-3 rounded-lg cursor-pointer font-medium text-lg'>Delete</button>
-    </div>
-    )
+    </>
+  )}</div>
+  );
 }
 
 export default Task
